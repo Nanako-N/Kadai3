55bc8b7e0a52ae4145887730a60429d71c5b85e5
NANAKO <NANAKO@192.168.15.43>
2018-05-10 03:34:49 +0900

    テストケースを記述/ソースコードを修正

--- test/snakecamel/SnakeCamelUtilTest.java
+++ test/snakecamel/SnakeCamelUtilTest.java
@@ -1,14 +1,58 @@
 package snakecamel;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
-
-class SnakeCamelUtilTest {
+public class SnakeCamelUtilTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void スネークからキャメルに変換できる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "AbcDefGhi";
+
+		String actual = scu.snakeToCamelcase("abc_def_ghi");
+
+		assertThat(actual, is(expected));
+
+		}
+	
+	public void キャメルからスネークに変換できる() {
+
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+
+		String expected = "abc_def_ghi";
+
+		String actual = scu.snakeToCamelcase("AbcDefGhi");
+
+		assertThat(actual, is(expected));
+
+		}
+
+		public void 先頭の文字を大文字に変換できる() {
+
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+
+		String expected = "Abc";
+
+		String actual = scu.snakeToCamelcase("abc");
+
+		assertThat(actual, is(expected));
+
+		}
+
+		 
+
+		public void 先頭の文字を小文字に変換できる() {
+
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+
+		String expected = "abc";
+
+		String actual = scu.snakeToCamelcase("Abc");
+
+		assertThat(actual, is(expected));
+
+		}
 
 }
